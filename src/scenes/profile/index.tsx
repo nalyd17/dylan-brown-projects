7935eee89019dd3bdf884a85fec88b29f96f7c95
nalyd17 <dylan.p.brown@gmail.com>
2024-10-15 13:39:52 -0500

    Update profile section to be mobile friendly

--- src/scenes/profile/index.tsx
+++ src/scenes/profile/index.tsx
@@ -3,13 +3,17 @@ import DylanHeadshot from "@/assets/DylanHeadshot.png";
 const Profile = () => {
   return (
     <section id="profile" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
-      <div className="mx-auto mt-12 w-5/6 items-center justify-center gap-16 md:flex">
+      <div
+        className={`mx-auto mt-12 grid w-5/6 grid-cols-1 gap-16 md:grid-cols-2`}
+      >
         <div>
           <img alt="dylan-headshot" src={DylanHeadshot}></img>
         </div>
-        <div className="flex w-[90%] flex-col gap-4 text-sm">
-          <p className="text-6xl">Dylan Brown</p>
-          <p className="text-2xl underline">Software Development Engineer</p>
+        <div className="flex w-[90%] flex-col justify-center gap-4 text-sm">
+          <div className="gap-1">
+            <p className="text-6xl">Dylan Brown</p>
+            <p className="text-2xl underline">Software Development Engineer</p>
+          </div>
           <p>
             Experienced Software Development Engineer with 5+ years of
             experience developing large-scale, production-quality software
