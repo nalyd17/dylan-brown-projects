3ceadb1b62d6e6e17a306b988d39873683d81ce0
nalyd17 <dylan.p.brown@gmail.com>
2024-10-15 10:28:37 -0500

    Finish navbar with mobile support

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,27 +1,31 @@
 import { SelectedPage } from "@/shared/types";
 import Link from "./Link";
 import useMediaQuery from "@/hooks/useMediaQuery";
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 
 type Props = {
+  isTopOfPage: boolean;
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between text-nowrap";
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full`}>
             <h6 className="w-full">Dylan Brown Projects</h6>
             {isAboveMediumScreens ? (
-              <div className={`flex w-full items-center justify-end gap-16`}>
+              <div className="flex w-full items-center justify-end gap-16">
                 <div className={`${flexBetween} gap-8 text-sm`}>
                   <Link
                     page="Profile"
@@ -64,6 +68,45 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           </div>
         </div>
       </div>
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          <div className="flex flex-col items-center gap-4 text-2xl">
+            <Link
+              page="Profile"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Work Experience"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Education"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Links"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+          <div className="flex justify-center py-4">
+            <button
+              className="rounded-md bg-secondary-500 px-6 py-2 hover:bg-primary-500 hover:text-white"
+              onClick={() => alert("TODO: Download Functionality")}
+            >
+              Download Resume
+            </button>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
